f3c9613980e095180651738188cfc09f1f103680
Jesse Wolgamott <jesse@comalproductions.com>
2013-01-29 22:01:24 -0600

    Add: better output for the custom matcher

--- spec/support/benchmark_spec.rb
+++ spec/support/benchmark_spec.rb
@@ -4,10 +4,22 @@ RSpec::Matchers.define :take_approximately do |n|
   chain :seconds do; end
 
   match do |block|
-    elapsed = Benchmark.realtime do
+    @elapsed = Benchmark.realtime do
       block.call
     end
-    elapsed.should be_within(0.2).of(n)
+    @elapsed.should be_within(0.2).of(n)
+  end
+
+  failure_message_for_should do |actual|
+    "expected block to take about #{expected} seconds, but took #{@elapsed}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected block to not take about #{expected} seconds, but took #{@elapsed}"
+  end
+
+  description do
+    "take approximately #{expectely} seconds to execute"
   end
 
 end
